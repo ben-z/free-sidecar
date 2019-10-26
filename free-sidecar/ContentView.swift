543d0161098b7d71125992ea9ae43c7b1340720a
Ben Zhang <benzhangniu@gmail.com>
2019-10-26 19:36:45 -0400

    Initial Commit

---
+++ free-sidecar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  free-sidecar
+//
+//  Created by Ben Zhang on 2019-10-26.
+//  Copyright © 2019 Ben Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ free-sidecarTests/free_sidecarTests.swift
@@ -0,0 +1,34 @@
+//
+//  free_sidecarTests.swift
+//  free-sidecarTests
+//
+//  Created by Ben Zhang on 2019-10-26.
+//  Copyright © 2019 Ben Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import free_sidecar
+
+class free_sidecarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
