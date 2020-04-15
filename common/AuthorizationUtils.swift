62f9185cfcc5eebabb5cc9a25116add9ada7957e
Ben Zhang <benzhangniu@gmail.com>
2020-04-14 20:37:47 -0400

    Allow Authorization to throw

--- common/AuthorizationUtils.swift
+++ common/AuthorizationUtils.swift
@@ -10,12 +10,17 @@ import Foundation
 import os.log
 
 public struct AuthorizationError: Error {
-    let osStatus: OSStatus
+    let osStatus: OSStatus?
     let description: String?
     
     init(_ osStatus: OSStatus) {
         self.osStatus = osStatus
         self.description = String(describing: SecCopyErrorMessageString(osStatus, nil))
+    }
+
+    init(_ description: String) {
+        self.osStatus = nil
+        self.description = description
     }
 }
 
@@ -27,40 +32,63 @@ public class Authorization {
     // Opaque pointers are non-nullable: https://stackoverflow.com/a/39979677/4527337
     //   so here we use ? to represent its NULL value
     var authRef: AuthorizationRef?
-    var err: AuthorizationError?
 
-    init(rights: [AuthorizationString], flags: AuthorizationFlags) {
+    init(rights: [AuthorizationString], flags: AuthorizationFlags) throws {
         var authItems = rights.map { AuthorizationItem(name: $0, valueLength: 0, value: nil, flags: 0) }
         let authItemsCount = authItems.count
-        authItems.withUnsafeMutableBufferPointer {
+        try authItems.withUnsafeMutableBufferPointer {
             var authRights = AuthorizationRights(count: UInt32(authItemsCount), items: $0.baseAddress)
             
-            do {
-                try executeAuthorizationFunction { AuthorizationCreate(&authRights, nil, flags, &authRef) }
-            } catch let error as AuthorizationError {
-                err = error
-            } catch {
-                os_log(.error, log: log, "An unknown error occured when creating an authorization: %{public}s", error.localizedDescription)
-            }
+            try executeAuthorizationFunction { AuthorizationCreate(&authRights, nil, flags, &authRef) }
         }
     }
     
-    func makeExternalForm() -> AuthorizationExternalForm? {
+    /**
+     @method init
+     @abstract Creates an Authorization object without rights, as described in https://developer.apple.com/library/archive/documentation/Security/Conceptual/authorization_concepts/03authtasks/authtasks.html#//apple_ref/doc/uid/TP30000995-CH206-BBCHCEEG
+     */
+    convenience init() throws {
+        try self.init(rights: [], flags: [])
+    }
+
+    /**
+     @method init
+     @abstract Creates an Authorization object from external from, as described in https://developer.apple.com/library/archive/documentation/Security/Conceptual/authorization_concepts/03authtasks/authtasks.html#//apple_ref/doc/uid/TP30000995-CH206-TPXREF14
+     */
+    convenience init(externalForm: AuthorizationExternalForm) throws {
+        try self.init()
+
+        var extForm = externalForm // mutable copy
+
+        try executeAuthorizationFunction { AuthorizationCreateFromExternalForm(&extForm, &authRef) }
+    }
+
+    /**
+     @method authorizeRights
+     @abstract use the AuthorizationCopyRights function to authorize/preauthorize rights
+     */
+    func authorizeRights(rights: [AuthorizationString], flags: AuthorizationFlags) throws {
         guard let ref = authRef else {
-            return nil
+            throw AuthorizationError("authRef is nil")
+        }
+        
+        var authItems = rights.map { AuthorizationItem(name: $0, valueLength: 0, value: nil, flags: 0) }
+        let authItemsCount = authItems.count
+        try authItems.withUnsafeMutableBufferPointer {
+            var authRights = AuthorizationRights(count: UInt32(authItemsCount), items: $0.baseAddress)
+
+            try executeAuthorizationFunction { AuthorizationCopyRights(ref, &authRights, nil, flags, nil) }
+        }
+    }
+
+    func makeExternalForm() throws -> AuthorizationExternalForm {
+        guard let ref = authRef else {
+            throw AuthorizationError("authRef is nil")
         }
         
         var extForm = AuthorizationExternalForm()
-        do {
-            try executeAuthorizationFunction { AuthorizationMakeExternalForm(ref, &extForm) }
-            return extForm
-        } catch let error as AuthorizationError {
-            err = error
-        } catch {
-            os_log(.error, log: log, "An unknown error occured when creating an authorization: %{public}s", error.localizedDescription)
-        }
-        
-        return nil
+        try executeAuthorizationFunction { AuthorizationMakeExternalForm(ref, &extForm) }
+        return extForm
     }
     
     deinit {
@@ -88,7 +116,7 @@ private func executeAuthorizationFunction(_ authorizationFunction: () -> (OSStat
  @abstract checks if the Authorization is accessible by creating an empty authorization
  */
 public func isAuthorizationAvailable() -> Bool {
-    let authorization = Authorization(rights: [], flags: [])
+    let authorization = try? Authorization(rights: [], flags: [])
     
-    return authorization.authRef != nil && authorization.makeExternalForm() != nil
+    return authorization?.authRef != nil && (try? authorization?.makeExternalForm()) != nil
 }
--- free-sidecar-xpc/FreeSidecarXPC.swift
+++ free-sidecar-xpc/FreeSidecarXPC.swift
@@ -12,22 +12,21 @@ import ServiceManagement
 
 class FreeSidecarXPCDelegate: NSObject, NSXPCListenerDelegate, FreeSidecarXPCProtocol {
     private let service = NSXPCListener.service()
-//    private let authState = AuthorizationState()
+    private let authorization: Authorization? = try? Authorization()
     
     override init() {
         super.init()
         service.delegate = self
-        
-        let authAvailable = isAuthorizationAvailable()
-        os_log(.debug, log: log, "isAuthorizationAvailable: %{public}s", String(authAvailable))
-        assert(authAvailable) // crash during developemnt
+
+        os_log(.debug, log: log, "Authorization available?: %{public}s", String(authorization != nil))
+        assert(authorization != nil) // crash during developemnt
     }
-    
+
     func run() {
         service.resume() // never returns
         os_log(.error, log: log, "service.resume() returned!")
     }
-    
+
     // MARK: -
     // MARK: NSXPCListenerDelegate protocol
     func listener(_ listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
@@ -39,7 +38,7 @@ class FreeSidecarXPCDelegate: NSObject, NSXPCListenerDelegate, FreeSidecarXPCPro
         newConnection.resume()
         return true
     }
-    
+
     // MARK: -
     // MARK: FreeSidecarXPCProtocol
     func upperCaseString(_ string: String, withReply reply: @escaping (String) -> Void) {
@@ -47,27 +46,29 @@ class FreeSidecarXPCDelegate: NSObject, NSXPCListenerDelegate, FreeSidecarXPCPro
         let response = string.uppercased()
         reply(response)
     }
-    
+
     func installHelper(withReply reply: @escaping (Error?) -> Void) {
-        withExtendedLifetime(Authorization(
-            rights: [kSMRightBlessPrivilegedHelper],
-            flags: [.interactionAllowed, .extendRights, .preAuthorize]))
-        { authorization in
-            guard let ref = authorization.authRef else {
-                os_log(.error, log: log, "Authorization error: %s", authorization.err!.localizedDescription)
-                reply(XPCError(.authUnavailable))
-                return
-            }
-            
-            var cfErr: Unmanaged<CFError>?
-    
-            if SMJobBless(kSMDomainSystemLaunchd, HELPER_BUNDLE_ID as CFString, ref, &cfErr) {
-                reply(nil)
-            } else if let err = cfErr?.takeRetainedValue() {
-                reply(err)
-            } else {
-                reply(XPCError(.unknownError))
-            }
+        guard let auth = authorization else {
+            reply(XPCError(.authUnavailable))
+            return
+        }
+
+        do {
+            try auth.authorizeRights(rights: [kSMRightBlessPrivilegedHelper], flags: [.interactionAllowed, .extendRights, .preAuthorize])
+        } catch {
+            os_log(.error, log: log, "Authorization error: %s", error.localizedDescription)
+            reply(XPCError(.authUnavailable))
+            return
+        }
+
+        var cfErr: Unmanaged<CFError>?
+
+        if SMJobBless(kSMDomainSystemLaunchd, HELPER_BUNDLE_ID as CFString, auth.authRef, &cfErr) {
+            reply(nil)
+        } else if let err = cfErr?.takeRetainedValue() {
+            reply(err)
+        } else {
+            reply(XPCError(.unknownError))
         }
     }
 }
